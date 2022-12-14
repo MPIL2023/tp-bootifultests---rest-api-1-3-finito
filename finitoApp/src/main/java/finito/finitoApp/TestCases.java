4495aff03eced3575044902f8f5c65723aceba5e
Elio <elliotfaugier@gmail.com>
2022-12-14 12:15:57 +0100

    test OK

--- finitoApp/src/main/java/finito/finitoApp/TestCases.java
+++ finitoApp/src/main/java/finito/finitoApp/TestCases.java
@@ -1,6 +1,5 @@
 package finito.finitoApp;
 
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,14 +7,10 @@ import static io.restassured.RestAssured.given;
 
 @SpringBootTest
 public class TestCases {
-
     int port=8080;
-
-
-
     @Test
     public void firstTestCase() {
-        String url = "http://localhost:8080/adder/add/?num=3";
+        String url = "http://localhost:8080/adder/current";
         given()
                 .when()
                 .get(url)
