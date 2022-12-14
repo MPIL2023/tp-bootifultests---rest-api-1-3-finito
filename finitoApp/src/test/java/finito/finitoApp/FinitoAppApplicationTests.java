9191806befccab64a2ed7ed7d1781ac3e99b9bd8
Elio <elliotfaugier@gmail.com>
2022-12-14 09:27:48 +0100

    init spinrg

---
+++ finitoApp/src/test/java/finito/finitoApp/FinitoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package finito.finitoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinitoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
