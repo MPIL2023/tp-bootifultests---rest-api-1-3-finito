e406481f8e7b21dbfae80c42cfc2e7285393529a
Elio <elliotfaugier@gmail.com>
2023-03-01 22:19:24 +0100

    2ème fix cors

--- finitoApp/src/main/java/finito/finitoApp/AdderController.java
+++ finitoApp/src/main/java/finito/finitoApp/AdderController.java
@@ -5,7 +5,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping(value = "/adder", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-@CrossOrigin("localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class AdderController {
 
